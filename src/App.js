b9367f539bf7964f9f7829d6c0661bd4a9da8fb4
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-07-25 00:21:06 +0300

    editing some content of the code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./index.css";
 function App() {
   return (
     <div className="bg-black text-white text-center">
-      <h1>Hello CodeSandbox</h1>
+      <h1>White Text</h1>
     </div>
   );
 }
